64c972937c7ed4e92fd32464ef1fb7c8e2b2a35a
Mark <peiqi1122@gmail.com>
2017-08-19 20:09:34 -0400

    Base64::decode + test

--- HtsgetServer/include/Codec.h
+++ HtsgetServer/include/Codec.h
@@ -1,6 +1,8 @@
 #ifndef ENCODING_H
 #define ENCODING_H
 
+// reference http://web.mit.edu/freebsd/head/contrib/wpa/src/utils/base64.c
+
 #include <string>
 #include <ostream>
 
@@ -13,29 +15,73 @@ auto inline operator<<(std::ostream& strm, byte_array in) -> std::ostream& {
 }
 
 
+
 class Base64Codec {
 
 private:
-    static constexpr unsigned char base64_table[] = 
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    static constexpr char base64_pad = '=';
-
-    static constexpr auto first6bit(const unsigned char* ptr) -> int {
+    /**
+     * @brief   Converts from a 3 1b (unsigned char) to 4 1b base64
+     */
+    static constexpr auto enc1(const unsigned char* ptr) -> int {
         return (ptr[0] >> 2);
     }
-    static constexpr auto second6bit(const unsigned char* ptr) -> int {
+    static constexpr auto enc2(const unsigned char* ptr) -> int {
         return ((ptr[0] & 0x03) << 4) | (ptr[1] >> 4);
     }
-    static constexpr auto third6bit(const unsigned char* ptr) -> int {
+    static constexpr auto enc3(const unsigned char* ptr) -> int {
         return ((ptr[1] & 0x0f) << 2) | (ptr[2] >> 6);
     }
-    static constexpr auto fourth6bit(const unsigned char* ptr) -> int {
+    static constexpr auto enc4(const unsigned char* ptr) -> int {
         return (ptr[2] & 0x3f);
     }
+
+    /**
+     * @brief   Converts from 4 1b base 64 (i.e. 0~63) to a 3 1b (unsigned char)
+     */ 
+    static constexpr auto dec1(const unsigned char* ptr) -> int {
+        return ((ptr[0] << 2) | (ptr[1] >> 4));
+    }
+    static constexpr auto dec2(const unsigned char* ptr) -> int {
+        return ((ptr[1] << 4) | (ptr[2] >> 2));
+    }
+    static constexpr auto dec3(const unsigned char* ptr) -> int {
+        return ((ptr[2] << 6) | ptr[3]);
+    }
+
+
+    static constexpr char base64_pad = '=';
+    static constexpr unsigned char encode_table[] = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    static constexpr unsigned char decode_table[256] =
+        {
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+                                                    // '+'              '/'
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
+        //  '0'                                 '9'             '='
+            52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64,  0, 64, 64,
+            //  'A'
+            64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
+            //  'a'
+            64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+            41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+            64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
+        };
+
 
 public:
     /**
      * @brief   Encode data to base64 encoding
+     * 
+     *          3 bytes (24 bits) -> 4 base64 character (6 bits)
      */
     auto static inline encode(const byte_array& input) -> byte_array {
         byte_array encoded;
@@ -47,25 +93,25 @@ public:
         auto end = ptr + input.size();
         
         while(end - ptr >= 3){
-            encoded += base64_table[first6bit(ptr)];
-            encoded += base64_table[second6bit(ptr)];
-            encoded += base64_table[third6bit(ptr)];
-            encoded += base64_table[fourth6bit(ptr)];
+            encoded += encode_table[enc1(ptr)];
+            encoded += encode_table[enc2(ptr)];
+            encoded += encode_table[enc3(ptr)];
+            encoded += encode_table[enc4(ptr)];
             ptr += 3;
         }
 
         switch(len % 3){
             case 1: {
-                encoded += base64_table[first6bit(ptr)];
-                encoded += base64_table[second6bit(ptr)];
+                encoded += encode_table[enc1(ptr)];
+                encoded += encode_table[enc2(ptr)];
                 encoded += base64_pad;
                 encoded += base64_pad;
                 break;
             }
             case 2: {
-                encoded += base64_table[first6bit(ptr)];
-                encoded += base64_table[second6bit(ptr)];
-                encoded += base64_table[third6bit(ptr)];
+                encoded += encode_table[enc1(ptr)];
+                encoded += encode_table[enc2(ptr)];
+                encoded += encode_table[enc3(ptr)];
                 encoded += base64_pad;
                 break;
             }
@@ -77,6 +123,45 @@ public:
     auto static inline encode(const std::string& in) -> byte_array {
         const auto input = byte_array(in.begin(), in.end());
         return encode(input);
+    }
+
+    /**
+     * @brief   Decode data from base64 encoding
+     * 
+     *          4 base64 character (6 bits) -> 3 bytes (24 bits)
+     */
+    auto static inline decode(const byte_array input) -> std::pair<byte_array, bool> {
+        
+        byte_array decoded;
+
+        if(input.size() % 4)
+            return make_pair(decoded, false);
+        
+        byte_array chunk4b;
+        chunk4b.reserve(4);
+        auto start = chunk4b.data();
+
+        auto ptr = input.begin();
+        while(ptr < input.end()){
+            
+            chunk4b[0] = decode_table[*ptr];
+            chunk4b[1] = decode_table[*(ptr + 1)];
+            chunk4b[2] = decode_table[*(ptr + 2)];
+            chunk4b[3] = decode_table[*(ptr + 3)];
+
+            decoded += dec1(start);
+            decoded += dec2(start);
+            decoded += dec3(start);
+
+            ptr += 4;
+        }
+
+        // remove trailing whitespace 
+        ptr = decoded.end();
+        while(*(ptr - 1) == 0){ --ptr; }
+        decoded.erase(ptr, input.end());
+
+        return make_pair(decoded, true);
     }
 
 };
--- HtsgetServer/test/test_Codec.cpp
+++ HtsgetServer/test/test_Codec.cpp
@@ -9,7 +9,7 @@
 using namespace Http;
 
 
-auto test_encoding(std::pair<std::string, std::string> in_out) -> void {
+auto test_codec(std::pair<std::string, std::string> in_out) -> void {
 
     auto in = std::get<0>(in_out);
     auto out = std::get<1>(in_out);
@@ -17,12 +17,26 @@ auto test_encoding(std::pair<std::string, std::string> in_out) -> void {
     auto input = byte_array(in.begin(), in.end());
     auto output = byte_array(out.begin(), out.end());
 
-    auto input_encoded = Base64Codec::encode(input);
-    REQUIRE(input_encoded == output);
+    // encoding 
+    auto encoded = Base64Codec::encode(input);
+    REQUIRE(encoded == output);
+    auto str_encoded = Base64Codec::encode(in);
+    REQUIRE(str_encoded == output);
 
-    auto in_encoded = Base64Codec::encode(in);
-    REQUIRE(in_encoded == output);
+    // decoding
+    byte_array decoded;
+    bool success;
+    std::tie(decoded, success) = Base64Codec::decode(output);   
+
+    // std::cout << "in: [" << output << "] decoded: [" << decoded << "] == [" << input << "]" << std::endl;
+    REQUIRE(success == true);
+    REQUIRE(decoded.size() == input.size());
+    REQUIRE(decoded == input);
+
 }
+
+
+
 
 
 TEST_CASE("base64 encoding/decoding", "[Codec]")
@@ -31,10 +45,11 @@ TEST_CASE("base64 encoding/decoding", "[Codec]")
    
     SECTION("base64 encoding")
     {
-        test_encoding({"pleasure.", "cGxlYXN1cmUu"});
-        test_encoding({"leasure.", "bGVhc3VyZS4="});
-        test_encoding({"easure.", "ZWFzdXJlLg=="});
-        test_encoding({"asure.", "YXN1cmUu"});
-        test_encoding({"sure.", "c3VyZS4="});
+        test_codec({"pleasure.", "cGxlYXN1cmUu"});
+        test_codec({"leasure.", "bGVhc3VyZS4="});
+        test_codec({"easure.", "ZWFzdXJlLg=="});
+        test_codec({"asure.", "YXN1cmUu"});
+        test_codec({"sure.", "c3VyZS4="});
     }
+
 }
